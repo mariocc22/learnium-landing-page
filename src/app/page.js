09d260fd9d3cf7dc27ae19692cc0e48a40f0393a
Mario Cesena <112787277+mariocc22@users.noreply.github.com>
2023-11-29 18:06:06 -0800

    feat: Adding title of website

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello World</h1>
+    <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-black">
+      <Image src="assets/in_progress.svg" alt="logo" width="500" height="500" />
+      <h1 className="text-white text-[32px] font-bold">
+        Landing Page In Progress...
+      </h1>
     </main>
   );
 }
